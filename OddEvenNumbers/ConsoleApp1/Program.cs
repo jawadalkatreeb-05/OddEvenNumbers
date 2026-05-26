ff82890389f530018912fec54299e8586a731cc1
jawad_alkatreeb <jawadalkatreeb@gmail.com>
2026-05-27 02:19:46 +0300

    Initial commit

---
+++ OddEvenNumbers/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            PrintNumbers("Numbers", a);
+            PrintNumbers("Even", a.Where(n => IsEven(n)));
+            PrintNumbers("Odd", a.Where(n => IsOdd(n)));
+
+            Console.ReadKey();
+        }
+        static void PrintNumbers(string title, IEnumerable<int> numbers)
+        {
+            Console.WriteLine();
+            Console.Write($"{title}: [");
+            foreach (var n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+            Console.Write($" ]");
+            Console.WriteLine();
+        }
+
+        static bool IsEven(int n)
+        {
+            return n % 2 == 0 ? true : false;
+        }
+        static bool IsOdd(int n)
+        {
+            return n % 2 !=  0 ? true : false;
+        }
+    }
+}
